130cb748814e4b1d0a0defac16d407ef2f8079b8
ikhyeons <skantrkwl789@naver.com>
2023-02-08 01:30:38 +0900

    글 리스트 페이지 ㄴ엄기기 구현 완료 / 10개까지만 나오게 수정해야 함

--- Server/Routers/document.ts
+++ Server/Routers/document.ts
@@ -5,16 +5,48 @@ const router = express.Router()
 import { getConnection } from '../dbConnection'
 
 //글 목록 보기
-router.get('/readDocList', async (req: Request, res: Response) => {
+router.get('/readDocList/:pageNum', async (req: Request, res: Response) => {
+  //페이지 넘버
+  const pageNum: number = Number(req.params.pageNum)
+  const startNum = 1 + 13 * (pageNum - 1) - 1
+  const endNum = 13
+
   //db연결을 위해 pool에서 커넥션을 대여함
   const connection = await getConnection()
   try {
     //데이터를 입력하는 쿼리
-    const [data] = await connection.query('select * from document', [])
+    const data = await connection.query(
+      'SELECT docNum, docTitle, docWriter, view, makeDate FROM document ORDER BY docNum DESC LIMIT ?, ?',
+      [startNum, endNum],
+    )
     //데이터 쿼리 종료 후 대여한 커넥션을 반납함
     connection.release()
     //결과가 성공이면 result 0과 데이터를 날림
+    console.log(data[0])
+    res.setHeader('content-type', 'application/json')
     res.send({ result: 0, data })
+  } catch (err) {
+    //db에서 에러나 났을 경우 커넥션을 반납하고
+    connection.release()
+    //에러로그를 출력함
+    console.log(err)
+    //프론트로 에러코드 result 3을 보냄
+    res.send({ result: 3 })
+  }
+})
+
+//글 갯수를 불러오는 라우팅
+router.get('/readDocCount/', async (req: Request, res: Response) => {
+  //db연결을 위해 pool에서 커넥션을 대여함
+  const connection = await getConnection()
+  try {
+    //데이터를 입력하는 쿼리
+    const [data]: any = await connection.query('SELECT COUNT(*) FROM document')
+    //데이터 쿼리 종료 후 대여한 커넥션을 반납함
+    connection.release()
+    //결과가 성공이면 result 0과 데이터를 날림
+    res.setHeader('content-type', 'application/json')
+    res.send({ result: 0, data: data[0]['COUNT(*)'] })
   } catch (err) {
     //db에서 에러나 났을 경우 커넥션을 반납하고
     connection.release()
