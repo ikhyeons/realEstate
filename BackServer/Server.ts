49feeb2553076faae24c92fdd3f74ef0fbbd7555
ikhyeons <skantrkwl789@naver.com>
2023-03-29 23:58:43 +0900

    연구실 배포 설정

--- BackServer/Server.ts
+++ BackServer/Server.ts
@@ -1,143 +1,137 @@
-import { NextFunction, Request, Response } from 'express'
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const http = require('http')
+import { NextFunction, Request, Response } from "express";
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const http = require("http");
 
-import mysqlSession from 'express-session'
-import { sessionConfig } from '../secretKeysB'
-import getConnection from './dbConnection'
-const createChatF = require('./Routers/chat')
+import mysqlSession from "express-session";
+import { sessionConfig } from "../secretKeysB";
+import getConnection from "./dbConnection";
+const createChatF = require("./Routers/chat");
 
-app.use(mysqlSession(sessionConfig))
+app.use(mysqlSession(sessionConfig));
 
-app.use(express.json())
+app.use(express.json());
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
-      'http://118.39.194.152:3000',
-      'http://203.232.200.71:3000',
-      'http://222.235.130.176:3000',
+      "http://localhost:3000",
+      "http://118.39.194.152:3000",
+      "http://203.232.200.71:3000",
+      "http://222.235.130.176:3000",
     ],
     credentials: true,
-  }),
-)
+  })
+);
 
-const server = http.createServer(app).listen(3010, function () {
-  console.log('Express server listening')
-})
+const server = http.createServer(app).listen(4004, function () {
+  console.log("Express server listening");
+});
 
-const ios = require('express-socket.io-session') // 소켓 내에서 세션데이터 접근 가능하도록하는 모듈
+const ios = require("express-socket.io-session"); // 소켓 내에서 세션데이터 접근 가능하도록하는 모듈
 
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: ["*"],
     credentials: true,
   },
-})
+});
 
-const docSocket = io.of('/doc')
-const chatSocket = io.of('/chat')
+const docSocket = io.of("/doc");
+const chatSocket = io.of("/chat");
 
-const session = require('./Routers/session')
-const user = require('./Routers/user')
-const releaseRoom = require('./Routers/releaseRoom')
-const document = require('./Routers/document')
-const reply = require('./Routers/reply')
-const chat = require('./Routers/chatRouter')
+const session = require("./Routers/session");
+const user = require("./Routers/user");
+const releaseRoom = require("./Routers/releaseRoom");
+const document = require("./Routers/document");
+const reply = require("./Routers/reply");
+const chat = require("./Routers/chatRouter");
 
-app.use('/session', session)
-app.use('/user', user)
-app.use('/releaseRoom', releaseRoom)
-app.use('/document', document)
-app.use('/reply', reply)
-app.use('/chat', chat)
+app.use("/session", session);
+app.use("/user", user);
+app.use("/releaseRoom", releaseRoom);
+app.use("/document", document);
+app.use("/reply", reply);
+app.use("/chat", chat);
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.sendFile(
-    'C:/Users/skant/OneDrive/Desktop/Projects/RealEstate/dist/index.html',
-  )
-})
+    "C:/Users/Administrator/Desktop/release/성익현/realEstate/dist/index.html"
+  );
+});
 
-app.get('/main.js', (req: Request, res: Response) => {
+app.get("/main.js", (req: Request, res: Response) => {
   res.sendFile(
-    'C:/Users/skant/OneDrive/Desktop/Projects/RealEstate/dist/main.js',
-  )
-})
+    "C:/Users/Administrator/Desktop/release/성익현/realEstate/dist/main.js"
+  );
+});
 
 //소켓 통신
 
-chatSocket.use(ios(mysqlSession(sessionConfig), { autoSave: true })) // 모듈과 세션 연결
-docSocket.use(ios(mysqlSession(sessionConfig), { autoSave: true })) // 모듈과 세션 연결
+chatSocket.use(ios(mysqlSession(sessionConfig), { autoSave: true })); // 모듈과 세션 연결
+docSocket.use(ios(mysqlSession(sessionConfig), { autoSave: true })); // 모듈과 세션 연결
 
-chatSocket.on('connection', async (socket: any) => {
-  const connection = await getConnection()
-  const [
-    data,
-  ]: any = await connection.query(
-    'select * from chatRoom where chatParticipant = ?',
-    [socket.handshake.session.Uid],
-  )
+chatSocket.on("connection", async (socket: any) => {
+  const connection = await getConnection();
+  const [data]: any = await connection.query(
+    "select * from chatRoom where chatParticipant = ?",
+    [socket.handshake.session.Uid]
+  );
   socket.join([
     ...data.map((data: any, i: number) => data.chatRoom),
     String(socket.handshake.session.Uid),
-  ])
+  ]);
 
-  socket.on('sendChat', async (rcv: any) => {
-    const [
-      data,
-    ]: any = await connection.query(
-      'select * from chatRoom where chatParticipant = ?',
-      [socket.handshake.session.Uid],
-    )
+  socket.on("sendChat", async (rcv: any) => {
+    const [data]: any = await connection.query(
+      "select * from chatRoom where chatParticipant = ?",
+      [socket.handshake.session.Uid]
+    );
     socket.join([
       ...data.map((data: any, i: number) => data.chatRoom),
       String(socket.handshake.session.Uid),
-    ])
+    ]);
 
-    await createChatF(rcv.roomNum, socket.handshake.session.Uid, rcv.data)
+    await createChatF(rcv.roomNum, socket.handshake.session.Uid, rcv.data);
 
-    socket.broadcast.to(rcv.roomNum).emit('sendChat', {
+    socket.broadcast.to(rcv.roomNum).emit("sendChat", {
       data: rcv.data,
       roomNum: rcv.roomNum,
       time: new Date(),
-    })
-  })
+    });
+  });
 
-  socket.on('createChat', async (rcv: any) => {
-    socket.join(String(rcv))
-    socket.broadcast.to(String(rcv)).emit('createChat')
-    socket.leave(String(rcv))
-  })
-})
+  socket.on("createChat", async (rcv: any) => {
+    socket.join(String(rcv));
+    socket.broadcast.to(String(rcv)).emit("createChat");
+    socket.leave(String(rcv));
+  });
+});
 
-docSocket.on('connection', async (socket: any) => {
-  const connection = await getConnection()
-  const [
-    data,
-  ]: any = await connection.query(
-    'SELECT * FROM document WHERE docWriter = ? AND del = 0',
-    [socket.handshake.session.Uid],
-  )
+docSocket.on("connection", async (socket: any) => {
+  const connection = await getConnection();
+  const [data]: any = await connection.query(
+    "SELECT * FROM document WHERE docWriter = ? AND del = 0",
+    [socket.handshake.session.Uid]
+  );
   socket.join([
     ...data.map((data: any, i: number) => data.docNum),
     String(socket.handshake.session.Uid),
-  ])
+  ]);
 
-  socket.on('writeReply', async (rcv: any) => {
-    socket.join(Number(rcv.docNum))
-    socket.broadcast.to(Number(rcv.docNum)).emit('writeReply', {
+  socket.on("writeReply", async (rcv: any) => {
+    socket.join(Number(rcv.docNum));
+    socket.broadcast.to(Number(rcv.docNum)).emit("writeReply", {
       data: rcv.data,
       docNum: rcv.docNum,
       time: new Date(),
-    })
-    socket.leave(Number(rcv.docNum))
-  })
+    });
+    socket.leave(Number(rcv.docNum));
+  });
 
-  socket.on('createReply', async (rcv: any) => {
-    socket.join(String(rcv))
-    socket.broadcast.to(String(rcv)).emit('createReply')
-    socket.leave(String(rcv))
-  })
-})
+  socket.on("createReply", async (rcv: any) => {
+    socket.join(String(rcv));
+    socket.broadcast.to(String(rcv)).emit("createReply");
+    socket.leave(String(rcv));
+  });
+});
