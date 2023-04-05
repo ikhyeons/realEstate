5199416ec8b5a638007859736988283579d806a1
ikhyeons <skantrkwl789@naver.com>
2023-04-05 18:47:35 +0900

    기존 코드 유지

--- BackServer/Server.ts
+++ BackServer/Server.ts
@@ -1,48 +1,48 @@
-import { NextFunction, Request, Response } from 'express'
-import { Socket } from 'socket.io'
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const http = require('http')
+import { NextFunction, Request, Response } from "express";
+import { Socket } from "socket.io";
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const http = require("http");
 
-import mysqlSession from 'express-session'
-import { sessionConfig } from '../secretKeysB'
-import getConnection from './dbConnection'
-import { FieldPacket, RowDataPacket } from 'mysql2'
-const createChatF = require('./Routers/chat')
+import mysqlSession from "express-session";
+import { sessionConfig } from "../secretKeysB";
+import getConnection from "./dbConnection";
+import { FieldPacket, RowDataPacket } from "mysql2";
+const createChatF = require("./Routers/chat");
 
 interface DocValue {
-  docNum: number | null
-  docTitle: string | ''
-  docContent: string | ''
-  userName: string | ''
-  makeDate: string | ''
-  view: number | null
-  docWriter: string
-  del?: number
+  docNum: number | null;
+  docTitle: string | "";
+  docContent: string | "";
+  userName: string | "";
+  makeDate: string | "";
+  view: number | null;
+  docWriter: string;
+  del?: number;
 }
 
 interface chatRoom extends RowDataPacket {
-  chatRoomNum: number
-  roomAddress: string
-  chatParticipant: number
-  chatOther: number
-  chatRoomroomMakeDate: string
+  chatRoomNum: number;
+  roomAddress: string;
+  chatParticipant: number;
+  chatOther: number;
+  chatRoomroomMakeDate: string;
 }
 
-type docValueMysql = DocValue & RowDataPacket
+type docValueMysql = DocValue & RowDataPacket;
 
 interface chatRcv {
-  roomNum: string
-  data: string
+  roomNum: string;
+  data: string;
 }
 
 interface replyRcv {
-  docNum: string
-  data: string
+  docNum: string;
+  data: string;
 }
 
-app.use(mysqlSession(sessionConfig))
+app.use(mysqlSession(sessionConfig));
 
 app.use(express.json());
 app.use(
@@ -57,15 +57,9 @@ app.use(
   })
 );
 
-<<<<<<< HEAD
 const server = http.createServer(app).listen(3001, function () {
-  console.log('Express server listening')
-})
-=======
-const server = http.createServer(app).listen(4004, function () {
   console.log("Express server listening");
 });
->>>>>>> 6ced4d79582782d7566d63f7dbcf1e21bb02237f
 
 const ios = require("express-socket.io-session"); // 소켓 내에서 세션데이터 접근 가능하도록하는 모듈
 
@@ -110,45 +104,22 @@ app.get("/main.js", (req: Request, res: Response) => {
 chatSocket.use(ios(mysqlSession(sessionConfig), { autoSave: true })); // 모듈과 세션 연결
 docSocket.use(ios(mysqlSession(sessionConfig), { autoSave: true })); // 모듈과 세션 연결
 
-<<<<<<< HEAD
-chatSocket.on('connection', async (socket: Socket) => {
-  const connection = await getConnection()
-  const [data]: [
-    chatRoom[],
-    FieldPacket[],
-  ] = await connection.query(
-    'select * from chatRoom where chatParticipant = ?',
-    [socket.handshake.session.Uid],
-  )
-=======
-chatSocket.on("connection", async (socket: any) => {
+chatSocket.on("connection", async (socket: Socket) => {
   const connection = await getConnection();
-  const [data]: any = await connection.query(
+  const [data]: [chatRoom[], FieldPacket[]] = await connection.query(
     "select * from chatRoom where chatParticipant = ?",
     [socket.handshake.session.Uid]
   );
->>>>>>> 6ced4d79582782d7566d63f7dbcf1e21bb02237f
   socket.join([
     ...data.map((data, i: number) => data.chatRoom),
     String(socket.handshake.session.Uid),
   ]);
 
-<<<<<<< HEAD
-  socket.on('sendChat', async (rcv: chatRcv) => {
-    const [data]: [
-      chatRoom[],
-      FieldPacket[],
-    ] = await connection.query(
-      'select * from chatRoom where chatParticipant = ?',
-      [socket.handshake.session.Uid],
-    )
-=======
-  socket.on("sendChat", async (rcv: any) => {
-    const [data]: any = await connection.query(
+  socket.on("sendChat", async (rcv: chatRcv) => {
+    const [data]: [chatRoom[], FieldPacket[]] = await connection.query(
       "select * from chatRoom where chatParticipant = ?",
       [socket.handshake.session.Uid]
     );
->>>>>>> 6ced4d79582782d7566d63f7dbcf1e21bb02237f
     socket.join([
       ...data.map((data, i: number) => data.chatRoom),
       String(socket.handshake.session.Uid),
@@ -163,75 +134,37 @@ chatSocket.on("connection", async (socket: any) => {
     });
   });
 
-<<<<<<< HEAD
-  socket.on('createChat', async (rcv) => {
-    socket.join(String(rcv))
-    socket.broadcast.to(String(rcv)).emit('createChat')
-    socket.leave(String(rcv))
-  })
-})
-
-docSocket.on('connection', async (socket: Socket) => {
-  const connection = await getConnection()
-  const [data]: [
-    docValueMysql[],
-    FieldPacket[],
-  ] = await connection.query(
-    'SELECT * FROM document WHERE docWriter = ? AND del = 0',
-    [socket.handshake.session.Uid],
-  )
-=======
-  socket.on("createChat", async (rcv: any) => {
+  socket.on("createChat", async (rcv) => {
     socket.join(String(rcv));
     socket.broadcast.to(String(rcv)).emit("createChat");
     socket.leave(String(rcv));
   });
 });
 
-docSocket.on("connection", async (socket: any) => {
+docSocket.on("connection", async (socket: Socket) => {
   const connection = await getConnection();
-  const [data]: any = await connection.query(
+  const [data]: [docValueMysql[], FieldPacket[]] = await connection.query(
     "SELECT * FROM document WHERE docWriter = ? AND del = 0",
     [socket.handshake.session.Uid]
   );
->>>>>>> 6ced4d79582782d7566d63f7dbcf1e21bb02237f
   socket.join([
     ...data.map((data, i: number) => String(data.docNum)),
     String(socket.handshake.session.Uid),
   ]);
 
-<<<<<<< HEAD
-  socket.on('writeReply', async (rcv: replyRcv) => {
-    socket.join(String(rcv.docNum))
-    socket.broadcast.to(String(rcv.docNum)).emit('writeReply', {
-      data: rcv.data,
-      docNum: rcv.docNum,
-      time: new Date(),
-    })
-    socket.leave(String(rcv.docNum))
-  })
-
-  socket.on('createReply', async (rcv: number) => {
-    socket.join(String(rcv))
-    socket.broadcast.to(String(rcv)).emit('createReply')
-    socket.leave(String(rcv))
-  })
-})
-=======
-  socket.on("writeReply", async (rcv: any) => {
-    socket.join(Number(rcv.docNum));
-    socket.broadcast.to(Number(rcv.docNum)).emit("writeReply", {
+  socket.on("writeReply", async (rcv: replyRcv) => {
+    socket.join(String(rcv.docNum));
+    socket.broadcast.to(String(rcv.docNum)).emit("writeReply", {
       data: rcv.data,
       docNum: rcv.docNum,
       time: new Date(),
     });
-    socket.leave(Number(rcv.docNum));
+    socket.leave(String(rcv.docNum));
   });
 
-  socket.on("createReply", async (rcv: any) => {
+  socket.on("createReply", async (rcv: number) => {
     socket.join(String(rcv));
     socket.broadcast.to(String(rcv)).emit("createReply");
     socket.leave(String(rcv));
   });
 });
->>>>>>> 6ced4d79582782d7566d63f7dbcf1e21bb02237f
