e7f350824328ca6265df9e61efdec67b181a2a46
ikhyeons <skantrkwl789@naver.com>
2023-04-12 18:19:06 +0900

    연구실 릴리즈용 브랜치

--- BackServer/Routers/chat.ts
+++ BackServer/Routers/chat.ts
@@ -1,28 +1,28 @@
-const express = require('express')
-import { getConnection } from '../dbConnection'
+const express = require("express");
+import { getConnection } from "../dbConnection";
 
 //채팅 작성하는 함수
 export const createChatF = async (
   chatRoomNum: string,
   userNum: number,
-  chatContent: string,
+  chatContent: string
 ) => {
-  const connection = await getConnection()
+  const connection = await getConnection();
   //데이터를 입력하는 쿼리
   try {
     await connection.query(
-      'insert into chat values (default, ?, ?, ?, default, default)',
-      [chatRoomNum, userNum, chatContent],
-    )
+      "insert into chat values (default, ?, ?, ?, default, default)",
+      [chatRoomNum, userNum, chatContent]
+    );
     //데이터 쿼리 종료 후 대여한 커넥션을 반납함
-    connection.release()
+    connection.release();
     //결과가 성공이면 result 0
   } catch (err) {
     //db에서 에러나 났을 경우 커넥션을 반납하고
-    connection.release()
-    console.log(err)
+    connection.release();
+    console.log(err);
     //에러로그를 출력함
   }
-}
+};
 
-module.exports = createChatF
+export default createChatF;
